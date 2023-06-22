edcb78e3869189630204d1551259e338d7004c11
NikhilaKumari-G <134587347+NikhilaKumari-G@users.noreply.github.com>
2023-06-22 11:29:36 +0530

    Create demo.tf

---
+++ demo.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAV5VYO6VGFJC5NO6R"
+  secret_key = "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"
+}
+
+/* resource "aws_instance" "this" {
+  ami                     = "ami-0715c1897453cabd1"
+  instance_type           = "t2.micro"
+} 
